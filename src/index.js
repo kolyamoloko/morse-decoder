f160fd90e74769f8e094edafdf446669e385c144
kolyamoloko <kolyamoloko@mail.ru>
2021-08-19 17:36:29 +0300

    add: function decoder

--- src/index.js
+++ src/index.js
@@ -35,12 +35,14 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ' // add space
 };
 
 function decode(expr) {
-    // write your solution here
+    const arr = [];//add array
+    for (let i = 0; i < expr.length; i += 10) {arr.push(expr.slice(i, i + 10));} // add multiple of 1
+    return arr.map(arr =>MORSE_TABLE[arr.replace(/^0+/, '').replace(/11/g, '-').replace(/10/g, '.')]).join('');//change symbols for morse-decoder
 }
-
 module.exports = {
     decode
 }
